ec2210cebe589f65225a642ff7897c47a3e02dea
asgard <1792181243@qq.com>
2022-10-04 00:16:24 +0800

    springboot_01 入门项目 springboot_02 基础配置

---
+++ springboot_01_quickStart/src/main/java/com/itheima/Springboot01QuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01QuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01QuickStartApplication.class, args);
+    }
+
+}
